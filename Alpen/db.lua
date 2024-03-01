318bbab803be1ae246e40458e04c993958e9d53f
Brian Sandford <b.d.sandford@gmail.com>
2024-03-01 00:38:55 -0800

    Add files via upload

--- Alpen/db.lua
+++ Alpen/db.lua
@@ -9,18 +9,35 @@ function db:new(t)
     return t
 end
 
-function database.openDatabase(filepath)
+function database.openDatabase(filepath, config_path)
     local instance = db:new()
     local db = {}
+
+    local config_file, config = io.open(config_path, "r"), {}
+    if config_file then
+        config = net.json2lua(config_file:read("*all"))
+    else
+        config =
+            net.json2lua([[
+                {
+                    "reset_time" : 43200,
+                    "starting_lives": 6,
+                    "helicopter_cost" : 1,
+                    "jet_cost" : 2
+                }
+                ]])
+    end
+    config_file:close()
     local file = io.open(filepath, "r")
     if file then
         file:close()
     else
         file = io.open(filepath, "w")
         if file then
-            local json = { players = {}, units = {}, objectives = {} }
+            local json = { players = {}, units = {}, objectives = {}, reset = os.time(), missionName = "" }
             json.units[1] = {}
             json.units[2] = {}
+            json.init = true
             file:write(net.lua2json(json))
             file:close()
         end
@@ -29,6 +46,7 @@ function database.openDatabase(filepath)
     local f = io.open(filepath, "r")
     instance.filepath = filepath
     instance.db = net.json2lua(f:read("*all"))
+    instance.config = config
     f:close()
     return instance
 end
@@ -54,7 +72,8 @@ function db:read()
         s = file:read("*all")
         file:close()
     end
-    return net.json2lua(s)
+    self.db = net.json2lua(s)
+    return self.db
 end
 
 function db:update()
@@ -62,22 +81,29 @@ function db:update()
     for ucid, player in next, current.players do
         self.db.players[ucid] = player
     end
+    self.db.units = current.units
+    self.db.objectives = current.objectives
 end
 
 function db:setMissionName(name)
-    self.missionName = name
+    self:read()
+    self.db.missionName = name
+    self:write()
 end
 
 function db:getPlayer(ucid)
+    self:read()
     return self.db.players[ucid]
 end
 
-function db:addPlayer(ucid)
+function db:addPlayer(ucid, side)
+    local current = self:read()
     if self:getPlayer(ucid) == nil then
         local player = {}
-        player.side = ""
-        player.lives = {}
+        player.side = side
+        player.lives = self.config.starting_lives
         self.db.players[ucid] = player
+        self:write()
         return true
     else
         return false
@@ -85,22 +111,46 @@ function db:addPlayer(ucid)
 end
 
 function db:removePlayer(ucid)
+    self:read()
     self.db.players[ucid] = nil
+    self:write()
 end
 
 function db:updatePlayers()
 end
 
+function db:getCategoryCost(category)
+    return self.config.lives_cost[category]
+end
+
 function db:getLives(ucid)
+    self:read()
     return self.db.players[ucid].lives
 end
 
-function db:subtractLife(type)
+function db:subtractLife(ucid, amt)
+    self:read()
+    self.db.players[ucid].lives = self.db.players[ucid].lives - amt
+    self:write()
 end
 
 function db:resetLives()
+    self:read()
     for ucid, playerTable in next, self.db.players do
+        self.db.players[ucid].lives = self.config["starting_lives"]
+    end
+    self.db.reset = os.time()
+    self:write()
+    log.write("Lives Reset", log.INFO, "Lives reset at " .. tostring(self.db.reset))
+    trigger.action.outText("Lives Reset!", 25)
+end
 
+function db:auditLifeTimer()
+    self:read()
+    local time = os.time()
+    local reset_time = self.config.reset_time
+    if time - reset_time > self.db.reset then
+        self:resetLives()
     end
 end
 
@@ -113,6 +163,7 @@ function db:saveGroup(g)
     group.coa = g:getCoalition()
     group.task = "Ground Nothing"
     group.units = {}
+    group.category = g:getCategory()
     for i, unit in next, g:getUnits() do
         group.units[i] = {}
         group.units[i].name = unit:getName()
@@ -122,15 +173,15 @@ function db:saveGroup(g)
         group.country = unit:getCountry()
     end
 
-    trigger.action.outText(self.filepath, 20, false)
-
     table.insert(self.db.units[group.coa], group)
 end
 
 function db:loadGroup(group)
+    coalition.addGroup(group.country, group.category , group )
 end
 
 function db:saveAllGroups()
+    self:read()
     for i = 1, 2 do
         self.db.units[i] = {}
         local groups = coalition.getGroups(i, 2)
@@ -138,29 +189,43 @@ function db:saveAllGroups()
             self:saveGroup(g)
         end
     end
+    self:write()
 end
 
 function db:loadAllGroups()
+    self:read()
+    for i = 1, 2 do
+        for groupName, group in next, self.db.units[i] do
+            self:loadGroup(group)
+        end
+    end
 end
 
 function db:startUpdateLoop(start_time, update_time)
     local function update(self, time)
-        self:update()
         self:saveAllGroups()
-        self:write()
+        self:auditLifeTimer()
         return time + update_time
     end
     timer.scheduleFunction(update, self, timer.getTime() + start_time)
 end
 
-function db:startUpdateLoopHooks(start_time, update_time)
+function db:startUpdateHooks()
     local function update(self, time)
-        self:update()
-        self:saveAllGroups()
-        self:write()
-        return time + update_time
+        return time
     end
-    timer.scheduleFunction(update, self, DCS.getModelTime() + start_time)
+    return self
+end
+
+function db:getInit()
+    self:read()
+    return self.db.init
+end
+
+function db:setInit(bool)
+    self:read()
+    self.db.init = bool
+    self:write()
 end
 
 return db, database
--- Alpen/entry.lua
+++ Alpen/entry.lua
@@ -1,7 +1,21 @@
 local db, database = dofile(lfs.writedir() .. "Alpen/db.lua")
 
-local test_filepath = lfs.writedir() .. "Alpen/unit_db.json"
+local test_filepath = lfs.writedir() .. "Alpen/db.json"
+local config_filepath = lfs.writedir() .. "Alpen/config.json"
 
-local unit_db = database.openDatabase(test_filepath)
+local unit_db = database.openDatabase(test_filepath, config_filepath)
+
+if unit_db:getInit() ~= true then
+    for i = 1, 2 do
+        local groups = coalition.getGroups(i , Group.Category.GROUND)
+        for _,g in next, groups do
+            log.write("Init Destroy", log.INFO, "destroying: " .. tostring(g:getName()))
+            g:destroy()
+        end
+    end
+    unit_db:loadAllGroups()
+else
+    unit_db:setInit(false)
+end
 
 unit_db:startUpdateLoop(1, 10)
