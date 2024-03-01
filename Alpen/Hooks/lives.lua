a20d2d334c9b563c50f6ec042d8337964d32aaab
Brian Sandford <b.d.sandford@gmail.com>
2024-03-01 00:40:23 -0800

    Add files via upload

--- Alpen/Hooks/lives.lua
+++ Alpen/Hooks/lives.lua
@@ -1,1 +1,53 @@
+local db, database = dofile(lfs.writedir() .. "Alpen/db.lua")
+local pw = "floggerz"
+local test_filepath = lfs.writedir() .. "Alpen/db.json"
+local config_filepath = lfs.writedir() .. "Alpen/config.json"
+local lives_db = database.openDatabase(test_filepath, config_filepath)
+local lives = {}
 
+local gameMasters = {
+    ["025ee29567ec00061db890812f4b8ec5"] = true, -- yink
+}
+
+local function split(pString, pPattern) --string.split
+    local Table = {}
+    local fpat = "(.-)" .. pPattern
+    local last_end = 1
+    local s, e, cap = pString:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(Table, cap)
+        end
+        last_end = e + 1
+        s, e, cap = pString:find(fpat, last_end)
+    end
+    if last_end <= #pString then
+        cap = pString:sub(last_end)
+        table.insert(Table, cap)
+    end
+    return Table
+end
+
+local function getUcid(pid)
+    return net.get_player_info(pid, 'ucid')
+end
+
+function lives.onPlayerTryChangeSlot(pid, coa, sid)
+    lives_db:read()
+    local lives = lives_db:getLives(getUcid(pid))
+    local category = DCS.getUnitProperty(sid, DCS.UNIT_GROUPCATEGORY)
+    local cost = lives_db:getCategoryCost(tostring(category))
+
+    net.send_chat_to("cost of ".. tostring(category) .. " is "  .. tostring(cost), pid)
+    net.send_chat_to("Lives remaining: ".. tostring(lives), pid)
+    if cost <= lives then
+        net.send_chat_to("Slotted!", pid)
+        return
+    else
+        net.send_chat_to("Not enough lives!", pid)
+    end
+
+    return false
+end
+
+DCS.setUserCallbacks(lives)
--- Alpen/Hooks/sidelocker.lua
+++ Alpen/Hooks/sidelocker.lua
@@ -1,14 +1,89 @@
 local db, database = dofile(lfs.writedir() .. "Alpen/db.lua")
+local pw = "floggerz"
+local test_filepath = lfs.writedir() .. "Alpen/db.json"
+local config_filepath = lfs.writedir() .. "Alpen/config.json"
+local hooks_db = database.openDatabase(test_filepath, config_filepath)
+local sideswitch = {}
 
-local test_filepath = lfs.writedir() .. "Alpen/hooks_db.json"
+local gameMasters = {
+    ["025ee29567ec00061db890812f4b8ec5"] = true, -- yink
+}
 
-local unit_db = database.openDatabase(test_filepath)
-
-local lives = {}
-
-function lives.onMissionLoadEnd()
-    unit_db:startUpdateLoopHooks(1, 10)
+local function split(pString, pPattern) --string.split
+    local Table = {}
+    local fpat = "(.-)" .. pPattern
+    local last_end = 1
+    local s, e, cap = pString:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(Table, cap)
+        end
+        last_end = e + 1
+        s, e, cap = pString:find(fpat, last_end)
+    end
+    if last_end <= #pString then
+        cap = pString:sub(last_end)
+        table.insert(Table, cap)
+    end
+    return Table
 end
 
-DCS.setUserCallbacks(lives)
+local function getUcid(pid)
+    return net.get_player_info(pid, 'ucid')
+end
 
+function sideswitch.onMissionLoadEnd()
+    hooks_db = database.openDatabase(test_filepath, config_filepath)
+    hooks_db:startUpdateHooks()
+end
+
+function sideswitch.onPlayerTryChangeSlot(pid, coa, sid)
+    if coa == 0 then return end
+
+    if hooks_db:getPlayer(getUcid(pid)) == nil then
+        net.send_chat_to("please type -red or -blue to pick a side!", pid)
+        return false
+    end
+
+    if hooks_db:getPlayer(getUcid(pid)).side == coa then
+        net.send_chat_to("Welcome back!", pid)
+        return
+    else
+        net.send_chat_to("You're not on that team!", pid)
+    end
+
+    return false
+end
+
+function sideswitch.onPlayerTrySendChat(pid, msg, toAll)
+
+    if hooks_db:getPlayer(getUcid(pid)) == nil then
+        if msg == "-red" then
+            hooks_db:addPlayer(getUcid(pid), 1)
+        elseif msg == "-blue" then
+            hooks_db:addPlayer(getUcid(pid), 2)
+        end
+    end
+    if gameMasters[getUcid(pid)] == true then
+        if msg == "-pids" then
+            local players = net.get_player_list()
+
+            for i, tpid in next, players do
+                net.send_chat_to(tostring(net.get_player_info(tpid, 'name')) .. " " .. tostring(getUcid(tpid)), pid)
+            end
+            
+            return ""
+        end
+        if msg == "-reset" then
+            hooks_db:reset()
+            return ""
+        end
+        if string.find(msg, "-rp") then
+            local ucid = split(msg, " ")[2]
+            hooks_db:removePlayer(ucid)
+            return ""
+        end
+    end
+end
+
+DCS.setUserCallbacks(sideswitch)
