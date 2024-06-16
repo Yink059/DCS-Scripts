ad3a6ce0aadf6b5b2b508fb270ded52c2a6076b7
Brian Sandford <b.d.sandford@gmail.com>
2024-06-16 15:57:12 -0700

    Add files via upload

--- Alpen/entry.lua
+++ Alpen/entry.lua
@@ -1,16 +1,27 @@
 local test_filepath = lfs.writedir() .. "Alpen/db.json"
 local config_filepath = lfs.writedir() .. "Alpen/config.json"
+local mission_table_path = lfs.writedir() .. "Alpen/mission.json"
 
 local db, database = dofile(lfs.writedir() .. "Alpen/db.lua")
 
-local unit_db = database.openDatabase(test_filepath, config_filepath)
+local unit_db = database.openDatabase(test_filepath, config_filepath, mission_table_path)
 
 if unit_db:getInit() ~= true then
     for i = 1, 2 do
         local groups = coalition.getGroups(i , Group.Category.GROUND)
         for _,g in next, groups do
             log.write("Init Destroy", log.INFO, "destroying: " .. tostring(g:getName()))
-            g:destroy()
+            local delete = true
+
+            for _, unit in next, g:getUnits() do
+                for _, ewrTypeName in next, unit_db.config.EWR do
+                    if unit:getTypeName() == ewrTypeName then
+                        delete = false
+                    end
+                end
+            end
+
+            if delete then g:destroy() end
         end
     end
     unit_db:loadAllGroups()
